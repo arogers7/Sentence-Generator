669762f3e6c3a9b28763f76fb870621500b935f5
Alison Rogers <arogers7@buffalo.edu>
2018-11-03 15:56:59 -0400

    working unigrams

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import nltk
+from nltk.tokenize import sent_tokenize
 
 def OpenBook(title):
     book = open(title + ".txt", "r")
@@ -8,3 +9,27 @@ def OpenBook(title):
 if __name__ == '__main__':
     title = "SignOfFour"
     bookRawText = OpenBook(title)
+    sentences = sent_tokenize(bookRawText)
+
+    unigrams = {}
+    bigrams = {}
+
+    for sentence in sentences:
+        tokens = nltk.word_tokenize(sentence)
+
+        for i in range(len(tokens) - 1):
+            word = tokens[i]
+            nextWord = tokens[i+1]
+            if word not in unigrams:
+                unigrams[word] = 1
+            else:
+                unigrams[word] += 1
+
+            if word not in bigrams:
+                bigrams[word] = {nextWord:1}
+            else:
+                pass
+                #bigrams[word][nextWord] += 1
+    print(bigrams['undivided'])
+
+    #print(bigrams)
